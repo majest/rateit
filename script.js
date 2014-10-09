070b0240f9226be069486a422ba51a8baf951c04
Artur Grabowski <majest.pl@gmail.com>
2014-10-09 15:07:03 +0100

    improved rating, preloading vote

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
+var rated = false;
+var url = "";
+var state = {};
+
+
+function setup() {
+    state = JSON.parse(getCookie('state')); 
+
+    rated = false;
+
+    // for all urls in state
+    for (k in state) {
+
+        // look for the one which matches the tab
+        if (k == url) {
+
+            // and go though all the ratings
+            for (rating in state[k]) {
+                $('.rating-' + rating).removeClass('rating-h');
+                // alnd change color of those ratings
+                for (i=1; i <= state[k][rating]; i++) {
+                    $('#' + rating + "-" + i).css('color','gold')
+                }
+            }
+
+            rated = true;
+             $('#send-button').hide();
+        }
+    }
+
+    if (!rated) {
+        $('#send-button').show();
+        $('.rating').each(function() {
+            $(this).addClass('rating-h')
+        });
+    }
+}
+
+
+
+chrome.tabs.getSelected(null,function(tab) {
+        var tablink = tab.url;
+        var parser = document.createElement('a');
+        parser.href = tablink;
+        url = parser.protocol + "//" + parser.hostname;
+        setup();
+});
 
 function createUUID() {
     // http://www.ietf.org/rfc/rfc4122.txt
@@ -14,23 +61,25 @@ function createUUID() {
     return uuid;
 }
 
-function setCookie(){
-    document.cookie = "id=" + createUUID();
-    return document.cookie;
-}
-
-function getCookie(){
-    return document.cookie;
-}
-
-var state = {};
-
 $(document).ready(function(){
+
+
     $('.rating span').click(function(span){
+
             var params = $(this).attr('id').split('-');
-            state[params[0]] = params[1];
-            for (i=1; i <= params[1]; i++) {
-               $('#' + params[0] + "-" + i).css('color','gold')
+
+            if (typeof(state[url]) == 'undefined') {
+                state[url] = {};
+            }
+
+            // if we didn't vote on this one yet
+            if (rated == false) {
+                state[url][params[0]] = parseInt(params[1]);
+                for (i=1; i <= params[1]; i++) {
+                   $('#' + params[0] + "-" + i).css('color','gold')
+                }
+
+                setCookie('state', JSON.stringify(state));
             }
     });
 
@@ -40,19 +89,31 @@ $(document).ready(function(){
 });
 
 
+
 function sendData() {
     chrome.tabs.getSelected(null,function(tab) {
-        var tablink = tab.url;
-        var parser = document.createElement('a');
-        parser.href = tablink;
-        url = parser.protocol + "//" + parser.hostname;
-        var cookie = getCookie();
-        if (cookie.length == 0) {
-            cookie = setCookie();
+
+        var uid = getCookie("id") 
+
+        if (uid == "") {
+            uid = createUUID();
+            setCookie("id", uid)
         }
 
-        cookieParts = cookie.split("-");
-        $.post('http://rateit.modeo.co.uk',{url: url, uid: cookie[1], data: {rating:state}})
+        if (typeof(state[url]) != 'undefined') {
+            var request = {url: url, uid: uid, data: {rating:state[url]}};
+
+            $.ajax('http://rateit.modeo.co.uk',{
+                'data': JSON.stringify(request), //{action:'x',params:['a','b','c']}
+                'type': 'POST',
+                'processData': false,
+                'contentType': 'application/json',
+                success: function(data){
+                    window.close();
+                }
+            });
+        }
+
     });
 }
 
